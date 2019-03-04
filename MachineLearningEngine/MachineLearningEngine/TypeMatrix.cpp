23b0c6b6bb9eb14cc647918072a180e5a9bff13d
Gronne <mathias0909@gmail.com>
2019-03-04 10:44:19 +0100

    Created the functions for the 'TypeMatrix' class. No functionality have been added yet

--- MachineLearningEngine/MachineLearningEngine/TypeMatrix.cpp
+++ MachineLearningEngine/MachineLearningEngine/TypeMatrix.cpp
@@ -4,9 +4,82 @@
 
 TypeMatrix::TypeMatrix()
 {
+
 }
 
 
 TypeMatrix::~TypeMatrix()
 {
+
 }
+
+
+bool TypeMatrix::dependent(const Matrix &)
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::homogene(const Matrix &)
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::basis(const Matrix &)
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::basis(Matrix &, MatrixSpace &) const
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::orthogonal(Matrix &, Matrix &) const
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::orthogonal(const Matrix *)
+{
+
+	return false;
+}
+
+
+int TypeMatrix::rank(const Matrix &)
+{
+
+	return 0;
+}
+
+
+int TypeMatrix::rank(const MatrixResult &)
+{
+
+	return 0;
+}
+
+
+bool TypeMatrix::checkSpan(Matrix &, Matrix &) const
+{
+
+	return false;
+}
+
+
+bool TypeMatrix::isomorphic(Matrix &, Matrix &) const
+{
+
+	return false;
+}
+
--- MachineLearningEngine/MachineLearningEngine/TypeMatrix.h
+++ MachineLearningEngine/MachineLearningEngine/TypeMatrix.h
@@ -1,8 +1,27 @@
 #pragma once
+#include "Matrix.h"
+#include "MatrixSpace.h"
+#include "MatrixResult.h"
+
 class TypeMatrix
 {
 public:
 	TypeMatrix();
 	~TypeMatrix();
+
+	bool dependent(const Matrix&);
+	bool homogene(const Matrix&);
+	bool basis(const Matrix&);
+	bool basis(Matrix&, MatrixSpace&) const;
+	bool orthogonal(Matrix&, Matrix&) const;
+	bool orthogonal(const Matrix*);
+	int rank(const Matrix&);
+	int rank(const MatrixResult&);
+	bool checkSpan(Matrix&, Matrix&) const;
+	bool isomorphic(Matrix&, Matrix&) const;
+
+private:
+
+
 };
 
