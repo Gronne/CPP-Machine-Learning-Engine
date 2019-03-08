337454e02ab97f94c1568d7bef7c727466e0c114
Gronne <mathias0909@gmail.com>
2019-03-08 08:36:46 +0100

    Added the prototype for the new class 'GetMatrix' that will have functions as 'get pivots' and 'get span'. No functionality have been implemented yet

---
+++ MachineLearningEngine/MachineLearningEngine/GetMatrix.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Matrix.h"
+#include "BasicMatrixOperations.h"
+
+class GetMatrix
+{
+public:
+	GetMatrix();
+	~GetMatrix();
+
+	int numberOfPivots(const Matrix&);
+	Matrix& pivotRows(const Matrix&);
+	Matrix& pivotColumns(const Matrix&);
+	Matrix& span(const Matrix&);
+	double span(const Matrix&, const Matrix&);
+	double innerProductSpace(const Matrix&);
+	Matrix& getBasicVectors(const Matrix&);
+	Matrix& getTransformationMatrix(const Matrix&, const Matrix&);
+
+private:
+
+};
+
