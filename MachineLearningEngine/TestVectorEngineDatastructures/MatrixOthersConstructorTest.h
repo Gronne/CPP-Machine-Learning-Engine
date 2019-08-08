c51edfccef2a16e544ff942dae9d562cad02a688
Gronne <mathias0909@gmail.com>
2019-08-08 21:43:12 +0200

    Added tests for the copy constructor in the MAtrix data object

--- MachineLearningEngine/TestVectorEngineDatastructures/MatrixOthersConstructorTest.h
+++ MachineLearningEngine/TestVectorEngineDatastructures/MatrixOthersConstructorTest.h
@@ -68,3 +68,102 @@ TEST(MatrixOthersConstructor, constructionExceptionMinusRowsAndColumns)
 	ASSERT_THROW(new Matrix(-5, -1), std::exception);
 	ASSERT_THROW(new Matrix(-1, -5), std::exception);
 }
+
+TEST(MatrixOthersConstructor, constructRefMatrix3x3NoInstiantion)
+{
+	Matrix *matrix = new Matrix(3, 3);
+
+	EXPECT_NO_THROW(new Matrix(*matrix));
+}
+
+TEST(MatrixOthersConstructor, constructRefMatrix3x3Instiantion)
+{
+	Matrix *matrix = new Matrix(3, 3);
+	*matrix = { 1, 2, 3,
+				4, 5, 6,
+				7, 8, 9 };
+
+	EXPECT_NO_THROW(new Matrix(*matrix));
+	EXPECT_TRUE(TypeMatrix::isEqual(new Matrix(*matrix), matrix));
+}
+
+TEST(MatrixOthersConstructor, constructRefMatrix1x1NoInstiantion)
+{
+	Matrix *matrix = new Matrix();
+
+	EXPECT_NO_THROW(new Matrix(*matrix));
+}
+
+TEST(MatrixOthersConstructor, constructRefMatrix2x3NoInstiantion)
+{
+	Matrix *matrix = new Matrix(2, 3);
+
+	EXPECT_NO_THROW(new Matrix(*matrix));
+}
+
+TEST(MatrixOthersConstructor, constructRefMatrix3x2NoInstiantion)
+{
+	Matrix *matrix = new Matrix(3, 2);
+
+	EXPECT_NO_THROW(new Matrix(*matrix));
+}
+
+TEST(MatrixOthersConstructor, constructRefMatrixMultiplication)
+{
+	Matrix *matrix = new Matrix(3, 3);
+	*matrix = { 1, 2, 3,
+				4, 5, 6,
+				7, 8, 9 };
+
+	EXPECT_NO_THROW(*matrix * (new Matrix(*matrix)));
+}
+
+
+TEST(MatrixOthersConstructor, constructPointerMatrix3x3NoInstiantion)
+{
+	Matrix *matrix = new Matrix(3, 3);
+
+	EXPECT_NO_THROW(new Matrix(matrix));
+}
+
+TEST(MatrixOthersConstructor, constructPointerMatrix3x3Instiantion)
+{
+	Matrix *matrix = new Matrix(3, 3);
+	*matrix = { 1, 2, 3,
+				4, 5, 6,
+				7, 8, 9 };
+
+	EXPECT_NO_THROW(new Matrix(matrix));
+	EXPECT_TRUE(TypeMatrix::isEqual(new Matrix(matrix), matrix));
+}
+
+TEST(MatrixOthersConstructor, constructPointerMatrix1x1NoInstiantion)
+{
+	Matrix *matrix = new Matrix();
+
+	EXPECT_NO_THROW(new Matrix(matrix));
+}
+
+TEST(MatrixOthersConstructor, constructPointerMatrix2x3NoInstiantion)
+{
+	Matrix *matrix = new Matrix(2, 3);
+
+	EXPECT_NO_THROW(new Matrix(matrix));
+}
+
+TEST(MatrixOthersConstructor, constructPointerMatrix3x2NoInstiantion)
+{
+	Matrix *matrix = new Matrix(3, 2);
+
+	EXPECT_NO_THROW(new Matrix(matrix));
+}
+
+TEST(MatrixOthersConstructor, constructPointerMatrixMultiplication)
+{
+	Matrix *matrix = new Matrix(3, 3);
+	*matrix = { 1, 2, 3,
+				4, 5, 6,
+				7, 8, 9 };
+
+	EXPECT_NO_THROW(*matrix * (new Matrix(matrix)));
+}
